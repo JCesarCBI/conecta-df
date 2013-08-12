22bdfa83226c86727dd4cf6cc8251928af9150a5
JCesarCBI <JCesarCBI@79a35d2b-5cda-d2d5-6ad2-a34b92ab0c6c>
2013-08-12 18:25:07 +0000

    12/08/13 Cesar Padilla Geobicacion actual del dispositivo y test de anotaciones multiples

--- Resources/search.js
+++ Resources/search.js
@@ -1,11 +1,57 @@
 var viewSearch = Ti.UI.createView();
 
-var labelTitle = Ti.UI.createLabel({
-	text: 'Buscar',
-	height: 'auto',
-	width: 'auto'
+Ti.Geolocation.accuracy = Ti.Geolocation.ACCURACY_BEST;
+Titanium.Geolocation.distanceFilter = 10;
+
+Ti.Geolocation.getCurrentPosition(function(e){
+	if (e.error)
+	{
+		alert('HFL cannot get your current location');
+		return;
+	}
+	
+    var longitude = e.coords.longitude;
+    var latitude = e.coords.latitude;
+    var altitude = e.coords.altitude;
+    var heading = e.coords.heading;
+    var accuracy = e.coords.accuracy;
+    var speed = e.coords.speed;
+    var timestamp = e.coords.timestamp;
+    var altitudeAccuracy = e.coords.altitudeAccuracy;  
+    
+    var stadiums = [
+    	Ti.Map.createAnnotation({
+    		latitude: 19.383391,
+    		longitude: -99.178285,
+    		title: 'Estadio Azul',
+    		animate: true,
+    		pincolor: Ti.Map.ANNOTATION_GREEN
+    	}),
+    	Ti.Map.createAnnotation({
+    		latitude: 19.30283,
+    		longitude: -99.150562,
+    		title: 'Estadio Azteca',
+    		animate: true,
+    		pincolor: Ti.Map.ANNOTATION_GREEN
+    	}),
+    	Ti.Map.createAnnotation({
+    		latitude: 19.331843,
+    		longitude: -99.192199,
+    		title: 'Estadio CU',
+    		animate: true,
+    		pincolor: Ti.Map.ANNOTATION_GREEN
+    	}),
+    ];
+    
+    var mapview = Titanium.Map.createView({
+        mapType: Titanium.Map.STANDARD_TYPE,
+        region: {latitude: latitude, longitude: longitude, latitudeDelta:0.01, longitudeDelta:0.01},
+        animate:true,
+        regionFit:true,
+        userLocation:true,
+        annotations: stadiums
+    });
+    
+    viewSearch.add(mapview);
+    Ti.UI.currentWindow.add(viewSearch);
 });
-
-viewSearch.add(labelTitle);
-
-Ti.UI.currentWindow.add(viewSearch);
