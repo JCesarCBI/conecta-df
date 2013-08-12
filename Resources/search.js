e48b36c43c4d88095ce11d11517707d5b473c6ad
JCesarCBI <JCesarCBI@79a35d2b-5cda-d2d5-6ad2-a34b92ab0c6c>
2013-08-12 23:04:57 +0000

    12/08/13 Cesar Padilla Anotaciones desde la base de datos

--- Resources/search.js
+++ Resources/search.js
@@ -10,48 +10,48 @@ Ti.Geolocation.getCurrentPosition(function(e){
 		return;
 	}
 	
-    var longitude = e.coords.longitude;
-    var latitude = e.coords.latitude;
-    var altitude = e.coords.altitude;
-    var heading = e.coords.heading;
-    var accuracy = e.coords.accuracy;
-    var speed = e.coords.speed;
-    var timestamp = e.coords.timestamp;
-    var altitudeAccuracy = e.coords.altitudeAccuracy;  
-    
-    var stadiums = [
-    	Ti.Map.createAnnotation({
-    		latitude: 19.383391,
-    		longitude: -99.178285,
-    		title: 'Estadio Azul',
-    		animate: true,
-    		pincolor: Ti.Map.ANNOTATION_GREEN
-    	}),
-    	Ti.Map.createAnnotation({
-    		latitude: 19.30283,
-    		longitude: -99.150562,
-    		title: 'Estadio Azteca',
-    		animate: true,
-    		pincolor: Ti.Map.ANNOTATION_GREEN
-    	}),
-    	Ti.Map.createAnnotation({
-    		latitude: 19.331843,
-    		longitude: -99.192199,
-    		title: 'Estadio CU',
-    		animate: true,
-    		pincolor: Ti.Map.ANNOTATION_GREEN
-    	}),
-    ];
-    
-    var mapview = Titanium.Map.createView({
-        mapType: Titanium.Map.STANDARD_TYPE,
-        region: {latitude: latitude, longitude: longitude, latitudeDelta:0.01, longitudeDelta:0.01},
-        animate:true,
-        regionFit:true,
-        userLocation:true,
-        annotations: stadiums
-    });
-    
-    viewSearch.add(mapview);
-    Ti.UI.currentWindow.add(viewSearch);
+	var request = Titanium.Network.createHTTPClient();
+	var url = "http://148.206.41.113/apparking.php?action=getParking"; 
+	request.open("GET",url);
+	request.onload = function (){
+		
+		var json = JSON.parse(this.responseText);
+		var dataArray = [];
+		var pos;
+		var annotationArray = [];
+		
+		for (pos=0; pos < json.length; pos++) {
+		  var annotation = Ti.Map.createAnnotation({
+		  	latitude: obj.latitud,
+		  	longitude: obj.longitud,
+		  	animate: true,
+		  	pincolor: Ti.Map.ANNOTATION_GREEN,
+		  	title: 'Estacionamiento'
+		  });
+		  
+		  annotationArray.push(annotation);
+		};
+		
+		var longitude = e.coords.longitude;
+	    var latitude = e.coords.latitude;
+	    var altitude = e.coords.altitude;
+	    var heading = e.coords.heading;
+	    var accuracy = e.coords.accuracy;
+	    var speed = e.coords.speed;
+	    var timestamp = e.coords.timestamp;
+	    var altitudeAccuracy = e.coords.altitudeAccuracy;  
+        
+	    var mapview = Titanium.Map.createView({
+	        mapType: Titanium.Map.STANDARD_TYPE,
+	        region: {latitude: latitude, longitude: longitude, latitudeDelta:0.01, longitudeDelta:0.01},
+	        animate:true,
+	        regionFit:true,
+	        userLocation:true,
+	        annotations: annotationArray
+	    });
+	    
+	    viewSearch.add(mapview);
+	    Ti.UI.currentWindow.add(viewSearch);
+	}    
 });
+
