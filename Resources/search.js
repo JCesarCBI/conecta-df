8e1362a5c0c846fa44b89cdff859b825bf31b6e3
JCesarCBI <JCesarCBI@79a35d2b-5cda-d2d5-6ad2-a34b92ab0c6c>
2013-08-14 01:13:00 +0000

    13/08/13 Cesar Padilla Anotaciones desde la base de datos XD

--- Resources/search.js
+++ Resources/search.js
@@ -1,57 +1,63 @@
 var viewSearch = Ti.UI.createView();
 
-Ti.Geolocation.accuracy = Ti.Geolocation.ACCURACY_BEST;
-Titanium.Geolocation.distanceFilter = 10;
+var url = "http://148.206.41.113/apparking.php?action=getParking";
+var gps = [];
+var json, coordinate, i, row;
 
-Ti.Geolocation.getCurrentPosition(function(e){
+Ti.Geolocation.accuracy = Ti.Geolocation.ACCURACY_BEST;
+Ti.Geolocation.distanceFilter = 10;
+
+Ti.Geolocation.getCurrentPosition(function(e)
+{
 	if (e.error)
 	{
 		alert('HFL cannot get your current location');
 		return;
 	}
 	
-	var request = Titanium.Network.createHTTPClient();
-	var url = "http://148.206.41.113/apparking.php?action=getParking"; 
-	request.open("GET",url);
-	request.onload = function (){
-		
-		var json = JSON.parse(this.responseText);
-		var dataArray = [];
-		var pos;
-		var annotationArray = [];
-		
-		for (pos=0; pos < json.length; pos++) {
-		  var annotation = Ti.Map.createAnnotation({
-		  	latitude: obj.latitud,
-		  	longitude: obj.longitud,
-		  	animate: true,
-		  	pincolor: Ti.Map.ANNOTATION_GREEN,
-		  	title: 'Estacionamiento'
-		  });
-		  
-		  annotationArray.push(annotation);
-		};
-		
-		var longitude = e.coords.longitude;
-	    var latitude = e.coords.latitude;
-	    var altitude = e.coords.altitude;
-	    var heading = e.coords.heading;
-	    var accuracy = e.coords.accuracy;
-	    var speed = e.coords.speed;
-	    var timestamp = e.coords.timestamp;
-	    var altitudeAccuracy = e.coords.altitudeAccuracy;  
-        
-	    var mapview = Titanium.Map.createView({
-	        mapType: Titanium.Map.STANDARD_TYPE,
-	        region: {latitude: latitude, longitude: longitude, latitudeDelta:0.01, longitudeDelta:0.01},
-	        animate:true,
-	        regionFit:true,
-	        userLocation:true,
-	        annotations: annotationArray
-	    });
-	    
-	    viewSearch.add(mapview);
-	    Ti.UI.currentWindow.add(viewSearch);
-	}    
+    var longitude = e.coords.longitude;
+    var latitude = e.coords.latitude;
+    var altitude = e.coords.altitude;
+    var heading = e.coords.heading;
+    var accuracy = e.coords.accuracy;
+    var speed = e.coords.speed;
+    var timestamp = e.coords.timestamp;
+    var altitudeAccuracy = e.coords.altitudeAccuracy;
+   
+    var xhr = Ti.Network.createHTTPClient({
+    	onload: function(){
+    		json = JSON.parse(this.responseText);
+    		var longitud = json.length;
+    		for(i=0; i<json.length; i++){
+    			coordinate = json[i];
+    			var annotation = Ti.Map.createAnnotation({
+    				latitude: coordinate.latitud,
+    				longitude: coordinate.longitud,
+    				animate: true,
+    				pincolor: Ti.Map.ANNOTATION_GREEN 
+    			});
+    			gps.push(annotation);
+    		}
+    		var parkinGPS = Titanium.Map.createView({
+    			mapType: Titanium.Map.STANDARD_TYPE,
+    			region: {latitude: latitude, longitude: longitude, latitudeDelta:0.01, longitudeDelta:0.01},
+    			animate:true,
+    			regionFit:true,
+    			userLocation:true,
+    			annotations: gps
+    			});
+    		viewSearch.add(parkinGPS);
+		},
+		onerror: function(e){
+			Ti.API.debug("STATUS: " + this.status);
+			Ti.API.debug("TEXT: " + this.responseText);
+			Ti.API.debug("ERROR" + e.error);
+		},
+		timeout:3000
+	});
+    
+    xhr.open("GET", url);
+	xhr.send();
+	
+	Ti.UI.currentWindow.add(viewSearch);
 });
-
